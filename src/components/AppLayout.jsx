63eb90a48be6dcef6e7029db9f0e28c7fc57711c
JF-Cozy <julien.ficheux@cozycloud.cc>
2025-05-27 10:45:59 +0200

    feat: Adjust titles

--- src/components/AppLayout.jsx
+++ src/components/AppLayout.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 
-import { BarComponent, BarCenter } from 'cozy-bar'
+import { BarComponent, BarCenter, BarLeft } from 'cozy-bar'
 import { useClient } from 'cozy-client'
+import AppTitle from 'cozy-ui/transpiled/react/AppTitle'
+import BarTitle from 'cozy-ui/transpiled/react/BarTitle'
 import { Layout, Main, Content } from 'cozy-ui/transpiled/react/Layout'
-import Typography from 'cozy-ui/transpiled/react/Typography'
 import Alerter from 'cozy-ui/transpiled/react/deprecated/Alerter'
 import useBreakpoints from 'cozy-ui/transpiled/react/providers/Breakpoints'
 import { useI18n } from 'cozy-ui/transpiled/react/providers/I18n'
@@ -19,14 +20,18 @@ const AppLayout = () => {
   return (
     <Layout>
       <BarComponent searchOptions={{ enabled: false }} />
+      {isMobile ? (
+        <BarCenter>
+          <BarTitle>{client.appMetadata.slug}</BarTitle>
+        </BarCenter>
+      ) : (
+        <BarLeft>
+          <AppTitle slug="home" />
+        </BarLeft>
+      )}
       {!isMobile && <Sidebar />}
       <Main>
         <Content>
-          {isMobile && (
-            <BarCenter>
-              <Typography variant="h5">{client.appMetadata.slug}</Typography>
-            </BarCenter>
-          )}
           <Outlet />
         </Content>
       </Main>
