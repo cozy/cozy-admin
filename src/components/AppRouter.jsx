62c72aaaca6c200a42b2194244364261db201f6f
JF-Cozy <julien.ficheux@cozycloud.cc>
2025-05-27 10:48:43 +0200

    refactor: Remove useless code

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -4,7 +4,6 @@ import { HashRouter, Routes, Route, Navigate } from 'react-router-dom'
 import { useBreakpoints } from 'cozy-ui/transpiled/react/providers/Breakpoints'
 
 import AppLayout from '@/components/AppLayout'
-import Contacts from '@/components/Views/Contacts'
 import Menu from '@/components/Views/Menu'
 import Welcome from '@/components/Views/Welcome'
 
@@ -17,7 +16,6 @@ const AppRouter = () => {
         <Route element={<AppLayout />}>
           {isMobile && <Route path="/menu" element={<Menu />} />}
           <Route path="/welcome" element={<Welcome />}></Route>
-          <Route path="/contacts" element={<Contacts />}></Route>
           <Route
             path="*"
             element={<Navigate replace to={isMobile ? '/menu' : '/welcome'} />}
--- src/queries/queries.js
+++ src/queries/queries.js
@@ -1,8 +1,1 @@
-import { Q } from 'cozy-client'
-
-export const buildContactsQuery = () => ({
-  definition: Q('io.cozy.contacts').limitBy(1000),
-  options: {
-    as: 'io.cozy.contacts/allContacts'
-  }
-})
+export default {}
